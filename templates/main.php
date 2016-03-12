10f1929ca83563aab2514cce465fec39be7cedb3
Niran Lohmaneeratana <niran.lohmaneeratana@tremontui.com>
2016-03-12 12:24:33 -0500

    Set up base for modular templating

--- templates/main.php
+++ templates/main.php
@@ -1,6 +1,19 @@
 <!DOCTYPE html>
+<html>
+	<title>
+		<?php 
+			//TITLE NAME
+		?>
+	</title>
+	<link rel="stylesheet" href="templates/style/<?php //THEME NAME ?>theme-base.css">
 <head>
 </head>
 <body>
-	testing
+	<?php
+		//foreach include for all forms and modules to include on page
+	?>
+	<p>
+		testing
+	</p>
 </body>
+</html>
