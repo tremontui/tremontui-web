58b89dbe7df9dc2ffcaa8650acfd4e627e138ede
Niran Lohmaneeratana <niran.lohmaneeratana@tremontui.com>
2016-03-12 20:00:49 -0500

    Added base styling.  Started Responsive Styles

--- templates/main.php
+++ templates/main.php
@@ -6,14 +6,39 @@
 		?>
 	</title>
 	<link rel="stylesheet" href="templates/style/<?php //THEME NAME ?>theme-base.css">
+	<link rel="stylesheet" href="templates/style/<?php //THEME NAME ?>theme-base-responsive.css">
 <head>
 </head>
 <body>
-	<?php
-		//foreach include for all forms and modules to include on page
-	?>
-	<p>
-		testing
-	</p>
+	<div id="wrapper">
+		<?php
+			//foreach include for all forms and modules to include on page
+		?>
+		<div class="blurb-mod">
+			<h1>
+				Top Header
+			</h1>
+			<h2>
+				Second Header
+			</h2>
+			<h3>
+				Third Header
+			</h3>
+			<h4>
+				Fourth Header
+			</h4>
+			<h5>
+				Fifth Header
+			</h5>
+			<h6>
+				Sixth Header
+			</h6>
+			<form>
+				<input name="username" placeholder="Username. . . ">
+				<input name="password" type="password" placeholder="Password. . . ">
+				<input type="submit" value="Log In">
+			</form>
+		</div>
+	</div>
 </body>
 </html>
